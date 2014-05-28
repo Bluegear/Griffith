44aca65e57f06514e4acdcb1071f51c5fff33975
Chatri Tung <gearmag@gmail.com>
2014-05-29 02:22:40 +0700

    Initial commit.

---
+++ src/test/java/org/bandofhawk/main/MainTest.java
@@ -0,0 +1,23 @@
+package org.bandofhawk.main;
+
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Bluegear on 5/29/14.
+ */
+public class MainTest extends TestCase {
+
+    Main main;
+
+    @Before
+    public void setUp() throws Exception {
+        main = new Main();
+    }
+
+    @Test
+    public void testSquare() throws Exception {
+        assertEquals(4, main.square(2));
+    }
+}
